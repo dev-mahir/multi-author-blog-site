45bf6425b0ca23c5c5f4d3a593e80ff14978df6c
Md Mahir <95497010+dev-mahir@users.noreply.github.com>
2023-02-21 19:35:27 +0600

    add vercel.json

--- api/middlewares/authMiddleware.js
+++ api/middlewares/authMiddleware.js
@@ -1,9 +1,11 @@
-
+import { tokenVerify } from "../utilis/token.js";
 
 export const authMiddleware = (req, res, next) => {
   const bearer_token = req.headers.authorization;
   const token = bearer_token.split(" ")[1];
 
-  console.log(token);
-  
-}
+  const checkToken = tokenVerify(token);
+  if (checkToken) {
+    console.log("ok");
+  }
+};
